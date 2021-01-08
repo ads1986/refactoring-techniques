6a08b80d4c3490d7ecb68b010e731093896f37a6
Anderson Martins <anderson.martins@BOMPMOM10276.local>
2021-01-08 17:26:24 -0300

    Implementing Inline Class.

---
+++ src/main/java/com/refactoring/techniques/moving/features/InlineClass.java
@@ -0,0 +1,76 @@
+package com.refactoring.techniques.moving.features;
+
+public class InlineClass {
+
+    /**
+     * Example before refactore.
+     */
+    class BeforeRefactore{
+
+        class Person {
+
+            private String name;
+            private TelephoneNumber telephoneNumber;
+
+            public Person(String name, TelephoneNumber telephoneNumber) {
+                this.name = name;
+                this.telephoneNumber = telephoneNumber;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public TelephoneNumber getTelephoneNumber() {
+                return telephoneNumber;
+            }
+        }
+
+        class TelephoneNumber{
+
+            public String officeAreaCode;
+            public String officeNumber;
+
+            public TelephoneNumber(String officeAreaCode, String officeNumber) {
+                this.officeAreaCode = officeAreaCode;
+                this.officeNumber = officeNumber;
+            }
+
+            public String getTelephoneNumber(){
+                return officeAreaCode + officeNumber;
+            }
+
+        }
+
+    }
+
+    /**
+     * Example after applying Inline Class technique
+     */
+    class AfterRefactore{
+
+        class Person {
+
+            private String name;
+            private String officeAreaCode;
+            private String officeNumber;
+
+            public Person(String name, String officeAreaCode, String officeNumber) {
+                this.name = name;
+                this.officeAreaCode = officeAreaCode;
+                this.officeNumber = officeNumber;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public String getTelephoneNumber(){
+                return officeAreaCode + officeNumber;
+            }
+
+        }
+
+    }
+
+}
